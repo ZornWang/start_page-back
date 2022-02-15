23ef617b88ce60806e8826b601fa41be4b1a6ac0
Zorn Wang <>
2022-02-15 14:51:20 +0800

    错误rollback

--- core/src/main/java/com/zorn/startpage/core/webgroup/controller/WebgroupController.java
+++ core/src/main/java/com/zorn/startpage/core/webgroup/controller/WebgroupController.java
@@ -43,9 +43,9 @@ public class WebgroupController {
     }
 
     //    删除组
-    @DeleteMapping("/{id}")
-    public Boolean remove(@PathVariable("id") Integer groupId) {
-        return webgroupService.removeById(groupId);
+    @DeleteMapping
+    public Boolean remove(@RequestBody Webgroup webgroup) {
+        return webgroupService.removeById(webgroup);
     }
 
     //    查询组内所有
